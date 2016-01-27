ed422bde260671df74d3e271d168047d85a94b55
Michael Schennum and Oskar Radon <oskarradonkimball@gmail.com>
2016-01-27 10:41:16 -0800

    added initial arry

---
+++ spec/specs.js
@@ -0,0 +1,20 @@
+// make array of words for computer to choose from
+describe('word', function() {
+  it("takes an array of words", function() {
+      expect(words[0]).to.eql("cat");
+      expect(words[1]).to.eql("man");
+  });
+});
+// have computer choose random word from array
+// display random word as blanks
+// make array of letters
+// check if letter is in word
+// check if game is won
+  // add letter to user array
+  // check fi user array equals answer array
+
+
+
+//jQuery
+//user clicks on letters
+//word is displayed
