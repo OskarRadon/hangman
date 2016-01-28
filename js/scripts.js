9899df865882d9e87b0c03424d58e1db172a34b7
Michael Schennum and Oskar Radon <oskarradonkimball@gmail.com>
2016-01-28 09:53:59 -0800

    fixes new game button to page reload

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,4 @@
 var words = ["cat", "internet", "initial", "look", "mat", "yak", "computer", "portland"];
-var counter = 0;
 
 function generateWord() {  //chooses word from array
   var wordIndex = Math.floor(8 * Math.random());
@@ -7,38 +6,44 @@ function generateWord() {  //chooses word from array
 };
 
 $(document).ready(function() {
-  $("form#hangman").submit(function(event) {
+
+
+    var counter = 0;
     var blankArray = [];
     var newWord = generateWord(words);
     console.log(newWord); // open the console to see the answer (to test)
-    for (i=0; i < newWord.length; i++) {
+    for (var i=0; i < newWord.length; i++) {
       blankArray.push("_ ");
     }
     $("#Result").html(blankArray);
     $("#letters-div a").click(function(e){
       var letterInput = $(e.target).text();
-      // var wrongGuess = newWord.indexOf(letterInput);
-      // if (wrongGuess === -1) {
-      //   counter = counter + 1;
-      //   console.log(counter);
-      //   if (counter === 6) {
-      //   console.log("You Lose!");
-      //   }
-      // }
+      var wrongGuess = newWord.indexOf(letterInput);
+      if (wrongGuess === -1) {
+        counter = counter + 1;
+        console.log(counter);
+      }
+      if (counter === 6) {
+      console.log("You Lose!");
+      }
       for(var j=0; j < newWord.length; j++) {
         var guess = newWord.indexOf(letterInput, j);
         if (guess !== -1) {
           blankArray.splice(guess, 1, letterInput);
         }
-        // var checkWord = blankArray.indexOf("_ ");
-        // if (checkWord === -1) {
-        //   console.log("You Win!");
-        // }
+
+      }
+      var checkWord = blankArray.indexOf("_ ");
+      if (checkWord === -1) {
+        console.log("You Win!");
       }
       $("#Result").html(blankArray);
 
     });
-    event.preventDefault();
-  });
 
+
+  });
+$("form#hangman").submit(function(event) {
+    window.location.reload();
+    event.preventDefault();
 });
