c3b76c9b6d3157db2ce97fa3370a7c8f663627db
Michael Schennum and Oskar Radon <mikeschenphoto@gmail.com>
2016-01-27 16:10:40 -0800

    Refactor and add comment

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
-var words = ["cat", "man", "hat", "car", "mat", "yak"];
+var words = ["cat", "man", "hat", "car", "mat", "yak", "computer", "portland"];
 var counter = 0;
 var userGuess = [];
 var results = [];
 
 function generateWord(words) {  //chooses word from array
-  var wordIndex = Math.floor(6 * Math.random());
+  var wordIndex = Math.floor(9 * Math.random());
   return words[wordIndex].split('');
 };
 
@@ -18,41 +18,35 @@ function compareWords() {
   return arrayContainsLetter;
 }
 
-function checkState() {
-  var gameState;
-  if ((counter < 7) && (userGuess === results)) {
-  gameState = "win";
-  } else if ((counter < 7) && (userGuess != results)) {
-  // do nothing
-  } else {
-  gameState = "loss";
-  }
-}
+// function checkState() {
+//   var gameState;
+//   if ((counter < 7) && (userGuess === results)) {
+//   gameState = "win";
+//   } else if ((counter < 7) && (userGuess != results)) {
+//   // do nothing
+//   } else {
+//   gameState = "loss";
+//   }
+// }
 
 $(document).ready(function() {
   $("form#hangman").submit(function(event) {
-  var blankArray = [];
-  var newWord = generateWord(words);
-  console.log(newWord);
-  for (i=0; i < newWord.length; i++) {
-    blankArray.push("_ ");
-  }
-  $("#Result").html(blankArray);
-  $("#letters-div a").click(function(e){
-    var letterInput = $(e.target).text();
-    var guess = newWord.indexOf(letterInput);
-    if (guess !== -1) {
-      blankArray.splice(guess, 1, letterInput);
+    var blankArray = [];
+    var newWord = generateWord(words);
+    console.log(newWord);
+    for (i=0; i < newWord.length; i++) {
+      blankArray.push("_ ");
+    }
+    $("#Result").html(blankArray);
+    $("#letters-div a").click(function(e){
+      var letterInput = $(e.target).text();
+      var guess = newWord.indexOf(letterInput);
+      console.log(guess);
+      if (guess !== -1) {
+        blankArray.splice(guess, 1, letterInput); //this doesn't work if a word has multiple of the same letter
+      }
       $("#Result").html(blankArray);
-    };
+    });
   });
   event.preventDefault();
- });
 });
-
-
-// gameWords.generateWord(gameWords.words);
-// gameWords.prototype.generateWord = function() {
-//   var wordIndex = Math.floor(6 * Math.random());
-//   return gameWords.words[wordIndex];
-// };
