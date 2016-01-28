8b983abe4520fd09d82c7f81d9af3acab7e03ef8
Michael Schennum and Oskar Radon <oskarradonkimball@gmail.com>
2016-01-28 13:48:19 -0800

    Make new Hangman obj

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,25 @@
-function Hangman (words, counter) {
+function Hangman (words, counter, blankArray) {
   this.words = words;
   this.counter = counter;
   this.blankArray = blankArray;
-}
-
-Hangman.prototype.generateWord() {  //chooses word from array
-  var wordIndex = Math.floor(8 * Math.random());
-  return words[wordIndex].split('');
 };
 
-Hangman.prototype.generateBlanks() {
-  var newWord = Hangman.generateWord(Hangman.words);
+var newHangman = new Hangman (["cat", "internet", "initial", "look", "mat", "yak", "computer", "portland"], 0, []);
+
+
+Hangman.prototype.generateWord = function() {  //chooses word from array
+  var wordIndex = Math.floor(8 * Math.random());
+  return newHangman.words[wordIndex].split('');
+};
+
+Hangman.prototype.generateBlanks = function() {
+  var newWord = newHangman.generateWord(newHangman.words);
   for (var i=0; i < newWord.length; i++) {
-    blankArray.push("_ ");
+    newHangman.blankArray.push("_ ");
   }
 };
 
-Hangman.prototype.searchBlanks(letterInput) {
+Hangman.prototype.searchBlanks = function(letterInput) {
   var wrongGuess = newWord.indexOf(letterInput);
   if (wrongGuess === -1) {
     counter = counter + 1;
@@ -25,29 +28,36 @@ Hangman.prototype.searchBlanks(letterInput) {
       return false;
     }
   }
-}
+};
 
-Hangman.prototype.replaceLetter() {
+Hangman.prototype.replaceLetter = function() {
   for(var j=0; j < newWord.length; j++) {
     var guess = newWord.indexOf(letterInput, j);
     if (guess !== -1) {
       blankArray.splice(guess, 1, letterInput);
     }
   }
-}
+};
 
-Hangman.counter = 0;
-Hangman.blankArray = [];
-Hangman.words = ["cat", "internet", "initial", "look", "mat", "yak", "computer", "portland"];
+Hangman.prototype.winState = function() {
+  var checkWord = blankArray.indexOf("_ ");
+  if (checkWord === -1) {
+    return true;
+  }
+};
+
+
+
+
 
 $(document).ready(function() {
-  Hangman.generateWord(Hangman.words);
-  Hangman.generateBlanks(Hangman.words);
-  $("#Result").html(blankArray);
+  newHangman.generateBlanks();
+  $("#Result").html(newHangman.blankArray);
 
   $("#letters-div a").click(function(e){
     var letterInput = $(e.target).text();
-    if (Hangman.searchBlanks(letterInput) === true) {
+    var search = newHangman.searchBlanks(letterInput);
+    if (newHangman.searchBlanks(letterInput) === true) {
       $("#myImage").empty();
       $("#myImage").append("<img src='img/" + (counter+1) + ".jpg'>");
     } else {
@@ -63,8 +73,8 @@ $(document).ready(function() {
       //     blankArray.splice(guess, 1, letterInput);
       //   }
       // }
-      var checkWord = blankArray.indexOf("_ ");
-      if (checkWord === -1) {
+      var win = newHangman.winState();
+      if (win === true) {
         $(".message").text("You Win!!!")
       }
       $("#Result").html(blankArray);
