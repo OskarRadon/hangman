ad147001a67c0a72d225817e629f437534c49dbd
Michael Schennum and Oskar Radon <mikeschenphoto@gmail.com>
2016-01-28 09:08:49 -0800

    fixes counter bug

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,10 @@
 var words = ["cat", "internet", "initial", "look", "mat", "yak", "computer", "portland"];
 var counter = 0;
+
 function generateWord() {  //chooses word from array
-  var wordIndex = Math.floor(9 * Math.random());
+  var wordIndex = Math.floor(8 * Math.random());
   return words[wordIndex].split('');
 };
-
-function compareWords() {
-  var arrayContainsLetter = (results.indexOf("a") > -1); //if user input is in results array = true
-  counter += 1;
-  return arrayContainsLetter;
-}
-
-// function checkState() {
-//   var gameState;
-//   if ((counter < 7) && (userGuess === results)) {
-//   gameState = "win";
-//   } else if ((counter < 7) && (userGuess != results)) {
-//   // do nothing
-//   } else {
-//   gameState = "loss";
-//   }
-// }
 
 $(document).ready(function() {
   $("form#hangman").submit(function(event) {
@@ -33,22 +17,26 @@ $(document).ready(function() {
     $("#Result").html(blankArray);
     $("#letters-div a").click(function(e){
       var letterInput = $(e.target).text();
-      var wrongGuess = newWord.indexOf(letterInput);
-      if (wrongGuess === -1) {
-        counter = counter + 1;
-        console.log(counter);
-      }
-      if (counter === 6) {
-        console.log("You Lose!");
-      }
-      for(j=0; j < newWord.length; j++) {
+      // var wrongGuess = newWord.indexOf(letterInput);
+      // if (wrongGuess === -1) {
+      //   counter = counter + 1;
+      //   console.log(counter);
+      //   if (counter === 6) {
+      //   console.log("You Lose!");
+      //   }
+      // }
+      for(var j=0; j < newWord.length; j++) {
         var guess = newWord.indexOf(letterInput, j);
         if (guess !== -1) {
           blankArray.splice(guess, 1, letterInput);
         }
+        // var checkWord = blankArray.indexOf("_ ");
+        // if (checkWord === -1) {
+        //   console.log("You Win!");
+        // }
       }
-      return counter;
       $("#Result").html(blankArray);
+
     });
     event.preventDefault();
   });
